caf9db617ab8fe9c72f00be61290f7d0878c6d6d
Abid Hasan <abidhasan.bh@outlook.com>
2024-10-02 23:52:10 +0600

    feat(FinanceChart): add a line chart component for finance overview

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
+import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard'
 import React from 'react'
 
@@ -27,7 +28,9 @@ const AdminPage = () => {
           </div>
         </div>
         {/* BOTTOM CHART   */}
-        <div className="w-full h-[500px]"></div>
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
         {/* RIGHT */}
       </div>
       <div className="w-full lg:w-1/3 flex flex-col gap-8">right</div>
