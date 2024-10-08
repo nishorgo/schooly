e75ce43836982494983192dd9ae8d291e9e6353e
Abid Hasan <abidhasan.bh@outlook.com>
2024-10-08 17:28:02 +0600

    change the meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Schoolio",
+  title: "Schooly",
   description: "A Fullfledged School Management System",
 };
 
